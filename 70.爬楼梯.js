feaa95ed240c29473331de78f3ad860bb7f958d2
Ruicong <crcong@foxmail.com>
2021-06-30 22:06:30 +0800

    feat: 增加斐波那契数列的递归版本

--- 70.爬楼梯.js
+++ 70.爬楼梯.js
@@ -11,19 +11,35 @@
  * @param {number} n
  * @return {number}
  */
+const map = {};
 var climbStairs = function(n) {
-    if (n == 1) {
+    // if (n == 1) {
+    //     return 1;
+    // }
+    // let first = 1;
+    // let second = 2;
+    // let result = 0;
+    // for (let i = 3; i <= n; i++) {
+    //     result = first + second;
+    //     first = second;
+    //     second = result;
+    // }
+    // return second;
+
+    // 递归+哈希表版本，不缓存过不了测试
+    if (n === 1) {
         return 1;
     }
-    let first = 1;
-    let second = 2;
-    let result = 0;
-    for (let i = 3; i <= n; i++) {
-        result = first + second;
-        first = second;
-        second = result;
+    if (n === 2) {
+        return 2;
     }
-    return second;
+    if (map[n]) {
+        return map[n];
+    } else {
+        const r = climbStairs(n - 1) + climbStairs(n - 2);
+        map[n] = r;
+    }
+    return map[n];
 };
 // @lc code=end
 
