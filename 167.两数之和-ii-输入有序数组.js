aa34340c628105b8ba547d65c4f23bfd872722f3
Ruicong <crcong@foxmail.com>
2021-07-10 23:00:27 +0800

    feat: 两数之和 II

---
+++ 167.两数之和-ii-输入有序数组.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=javascript
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+    while (left < right) {
+        if (numbers[left] + numbers[right] > target) {
+            right--;
+        } else if (numbers[left] + numbers[right] < target) {
+            left++;
+        } else {
+            return [left + 1, right + 1];
+        }
+    }
+    return [-1, -1];
+};
+// @lc code=end
+
