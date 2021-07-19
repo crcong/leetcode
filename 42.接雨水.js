3ca829b83a1860266fc948d54a61cbfaa914ec4b
Ruicong <crcong@foxmail.com>
2021-07-19 23:01:59 +0800

    feat: 42.接雨水(单调栈自己写的粗糙版)

---
+++ 42.接雨水.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=javascript
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+    const stack = [];
+    let result = 0;
+    for (let i = 0; i < height.length; i++) {
+        let pops = [];
+        while (stack.length > 0 && stack[stack.length - 1][1] > height[i]) {
+            pops.push(stack.pop());
+        }
+        if (stack[stack.length - 1]) {
+            pops.push(stack[stack.length - 1]);
+        }
+        pops = pops.reverse();
+        for (let j = 0; j < pops.length; j++) {
+            const pop = pops[j];
+            if (pops[j + 1]) {
+                const min = Math.min(height[i], height[pop[0]])
+                result += (min - pops[j + 1][1]) * (i - pop[0] - 1);
+            }
+        }
+        stack.push([i, height[i]]);
+    }
+    return result;
+};
+// @lc code=end
+
